0c327d763da10b0c23b7126cf4b77bca60e3fbc1
Luke <lukem_95@hotmail.com>
2015-12-01 13:57:45 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(function (req, res) {
+	res.sendFile('./public/lr.html')
+});
+
+var server = app.listen(3000, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Literary Rain listening at http://%s:%s', host, port);
+});
