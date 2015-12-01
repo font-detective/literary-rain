3b8dac703021f30ab4f2503e03c8178d95b0efb5
Luke <lukem95@gmail.com>
2015-12-01 14:49:05 +0000

    Working file upload

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/upload', function (req, res) {
+	var base64Data = req.body.imgBase64.replace(/^data:image\/png;base64,/, "");
+
+	require("fs").writeFile("./output/out.png", base64Data, 'base64', function(err) {
+		if (err) {
+			console.error(err);	
+		}
+	});
+})
 
 app.use(function (req, res) {
 	res.sendFile('./public/lr.html', { root : __dirname});
